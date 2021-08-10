a2f6349951cea050f2184dfcf0b36818a129b276
Yunhong Min <yunongmin@gmail.com>
2021-08-11 00:22:35 +0900

    completed Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,57 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// save initial cursor (SCREEN pointer) to R0
+@SCREEN
+D=A
+@R0
+M=D
+
+(LOOP)
+@KBD
+D=M
+
+// if keyboard is on, go to ON
+@ON
+D;JGT
+
+
+// if keyboard is off, go to OFF
+@OFF
+0;JMP
+
+
+(ON)
+// fill the area on cursor with black
+@R0
+A=M
+M=-1
+
+// increase the cursor by 1
+@R0
+M=M+1
+
+@LOOP
+0;JMP
+
+
+(OFF)
+// check if cursor is at SCREEN, no job required
+@R0
+D=M
+@SCREEN
+D=D-A
+@LOOP
+D;JEQ
+
+
+// fill the area on cursor with white
+@R0
+M=M-1
+
+A=M
+M=0
+
+@LOOP
+0;JMP
+
